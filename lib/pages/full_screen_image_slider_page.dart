fb2539165b28efae308e9196d5d6abddbc6126c1
Чіжмодій Іван <71609765+chizmodiy@users.noreply.github.com>
2025-08-11 12:09:22 +0300

    feat: додано навігаційні стрілки для повноекранного перегляду зображень
    - Додано кнопки стрілок для навігації між зображеннями у повноекранному режимі.
    - Реалізовано новий метод _buildNavigationArrow для створення кнопок навігації з відповідною логікою переходу.
    - Збільшено відступи для кнопок навігації для покращення візуального сприйняття.

--- lib/pages/full_screen_image_slider_page.dart
+++ lib/pages/full_screen_image_slider_page.dart
@@ -63,6 +63,26 @@ class _FullScreenImageSliderPageState extends State<FullScreenImageSliderPage> {
           // Close button
           Positioned(top: 40, right: 20, child: _buildCloseButton(context)),
 
+          // Left arrow button
+          Positioned(
+            left: 30, // Increased padding from edge
+            top: MediaQuery.of(context).size.height / 2 - 24, // Center vertically
+            child: _buildNavigationArrow(
+              Icons.arrow_back_ios,
+              _currentIndex > 0 ? () => _pageController.previousPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn) : null,
+            ),
+          ),
+
+          // Right arrow button
+          Positioned(
+            right: 30, // Increased padding from edge
+            top: MediaQuery.of(context).size.height / 2 - 24, // Center vertically
+            child: _buildNavigationArrow(
+              Icons.arrow_forward_ios,
+              _currentIndex < widget.imageUrls.length - 1 ? () => _pageController.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.easeIn) : null,
+            ),
+          ),
+
           // Image counter
           Positioned(
             bottom: 40,
@@ -95,6 +115,22 @@ class _FullScreenImageSliderPageState extends State<FullScreenImageSliderPage> {
           border: Border.all(color: AppColors.white.withOpacity(0.3)), // Subtle white border
         ),
         child: const Icon(Icons.close, color: Colors.white, size: 24),
+      ),
+    );
+  }
+
+  // New method for navigation arrows
+  Widget _buildNavigationArrow(IconData icon, VoidCallback? onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(8.0), // Symmetric padding for both icons
+        decoration: BoxDecoration(
+          color: AppColors.white.withOpacity(0.2), // Same style as close button
+          shape: BoxShape.circle,
+          border: Border.all(color: AppColors.white.withOpacity(0.3)),
+        ),
+        child: Icon(icon, color: onTap != null ? Colors.white : Colors.white.withOpacity(0.5), size: 24),
       ),
     );
   }
